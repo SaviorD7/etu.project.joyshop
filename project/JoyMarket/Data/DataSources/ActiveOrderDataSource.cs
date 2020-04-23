752dfdc2e2085c35f22f183ac003ac0fd0e0c637
SaviorD7 <saviord7@gmail.com>
2020-04-23 04:25:56 +0300

    project

---
+++ project/JoyMarket/Data/DataSources/ActiveOrderDataSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.DataSources
+{
+    public partial class ActiveOrderDataSource
+    {
+        public int OrderId { get; set; }
+        public int JoySellId { get; set; }
+        
+        public OrderDataSource Order { get; set; }
+        public JoySellDataSource JoySell { get; set; }
+        public DateTime OrderTime { get; set; }
+    }
+}
---
+++ project/JoyMarket/Data/DataSources/SelectedCompositionDataSource.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.DataSources
+{
+    public partial class SelectedCompositionDataSource
+    {
+        public int JoySellId { get; set; }
+        public int CompositionId { get; set; }
+        
+        public JoySellDataSource JoySell { get; set; }
+        public CompositionDataSource Composition { get; set; }
+    }
+}
